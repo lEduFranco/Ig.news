2033b521af8cba229d8eda98bc189a7681097401
Luiz Eduardo Franco <60003877+lEduFranco@users.noreply.github.com>
2021-06-07 22:40:42 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Duline</title>
+      </Head>
+
+      <h1>
+        hello word!
+      </h1>
+    </>
+  );
+}
